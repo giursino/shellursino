0ee9e941584419777d2c2a6b33c0ddc87b407f6b
Giuseppe Ursino <giursino@gmail.com>
2018-05-06 15:02:14 +0200

    aggiunto controllo avvio calibre

--- bin/backup-calibre.sh
+++ bin/backup-calibre.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+function stop-calibre {
+	echo "ATTENZIONE: questo script non può essere eseguito se Calibre è avviato!"
+	exit 1
+}
+
+echo "Controllo assenza Calibre..."
+pgrep -x calibre && stop-calibre
+
+echo "Inizio la sincronizzazione..."
+exit 0
+
 rsync -av --delete -e ssh $HOME/Biblioteca/ "giursino@nasgg::ebook/Romanzi/Biblioteca di Calibre - READ ONLY/"
