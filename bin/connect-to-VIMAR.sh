dd7238ce2af23ba454a6ae77f171fdcb238189d4
Giuseppe Ursino <giursino@gmail.com>
2018-07-11 22:33:44 +0200

    fix script VPN

--- bin/connect-to-VIMAR.sh
+++ bin/connect-to-VIMAR.sh
@@ -11,6 +11,13 @@ sudo ip route del 192.168.1.8
 sudo ip route del 192.168.1.10/31
 sudo ip route del 192.168.1.16
 
+echo "Removing 192.168.1.x route from VIMAR VPN..."
+sudo ip route del 192.168.1.1
+sudo ip route del 192.168.1.7
+sudo ip route del 192.168.1.8
+sudo ip route del 192.168.1.10/31
+sudo ip route del 192.168.1.16
+
 echo "Connecting to development machine..."
 ssh -Y ursinog@vimar.net@172.20.32.15
 
