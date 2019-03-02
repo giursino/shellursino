080ca9ae1dc8a6988f6acbecb26c116fbad6efea
Giuseppe Ursino <giursino@gmail.com>
2019-03-02 17:41:53 +0100

    change mount cifs

--- bin/umount-NET.sh
+++ bin/umount-NET.sh
@@ -2,6 +2,8 @@
 # force to umount network disk
 
 echo "Wait until umount network disk is finish..."
-umount -l /net/*
+CIFS=$(mount | grep cifs | awk '{ print $3 }')
+echo $CIFS
+sudo umount -l ${CIFS}
 echo "Done!"
 
